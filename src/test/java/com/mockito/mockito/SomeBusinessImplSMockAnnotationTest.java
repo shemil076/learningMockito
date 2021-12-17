b7457c30974abd5b4531a696220971a3ca3e3f68
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-18 00:07:17 +0530

    Using Mockito Annotation- @Mock @InjectMock, @RunWith

---
+++ src/test/java/com/mockito/mockito/SomeBusinessImplSMockAnnotationTest.java
@@ -0,0 +1,29 @@
+package com.mockito.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class);
+public class SomeBusinessImplSMockAnnotationTest {
+
+    @Mock
+    DataService dataServiceMock;
+
+    @InjectMocks
+    SomeBusinessImpl businessImpl;
+
+
+    @Test
+   public void findTheGreatestNumber() {
+        when(dataServiceMock.retirieveAllData()).thenReturn(new int[]{24,15,3});
+         int result = businessImpl.findTheGreatestNumber();
+         assertEquals(24,result);
+    }
+}
